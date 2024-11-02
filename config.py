7387689f6f08b43e8ff249442981d231fad52f06
TSOlami <tijani.saheed@yahoo.com>
2024-11-02 16:04:49 +0100

    Add OPENAI_API_KEY to configuration for environment variable loading

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ load_dotenv()
 class Config:
     ARIA_API_KEY=os.environ.get('ARIA_API_KEY')
     ALLEGRO_API_KEY=os.environ.get('ALLEGRO_API_KEY')
+    OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
